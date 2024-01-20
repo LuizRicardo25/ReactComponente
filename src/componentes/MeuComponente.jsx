2c8ce5538d885fee9a7616305a143133ff8ab7ee
Luiz Ricardo Mantovani da Silva <mantovani36@gmail.com>
2024-01-20 18:44:03 -0300

    Primeiro Commit - Componente com classe

---
+++ src/componentes/MeuComponente.jsx
@@ -0,0 +1,16 @@
+// Importe o React
+import React, { Component } from 'react';
+
+// Crie a Classe do Componente
+class MeuComponente extends Component {
+  render() {
+    return (
+      <div>
+        Olá, este é o meu componente!
+      </div>
+    );
+  }
+}
+
+// Exporte o Componente
+export default MeuComponente;
